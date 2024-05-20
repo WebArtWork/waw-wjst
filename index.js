6a65874618f055974af1ff7c06bd3f75c497bc07
Narolskyi Denis <asotizianov@gmail.com>
2024-05-20 11:29:57 +0300

    added build on template to prepare it for gh pages

--- index.js
+++ index.js
@@ -1,50 +1,107 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const sep = path.sep;
-module.exports = function(waw){
-	waw.derer.setFilter('translate', (phrase) => {
+module.exports = function (waw) {
+	waw.derer.setFilter("translate", (phrase) => {
 		return phrase;
 	});
-	waw.wjst.setFilter('mongodate', function (_id) {
+	waw.wjst.setFilter("mongodate", function (_id) {
 		if (!_id) return new Date();
 		let timestamp = _id.toString().substring(0, 8);
 		return new Date(parseInt(timestamp, 16) * 1000);
 	});
 	let template = {};
-	if (fs.existsSync(process.cwd() + sep + 'template.json')) {
-		template = JSON.parse(fs.readFileSync(process.cwd() + sep + 'template.json'));
+	if (fs.existsSync(process.cwd() + sep + "template.json")) {
+		template = JSON.parse(
+			fs.readFileSync(process.cwd() + sep + "template.json")
+		);
 	}
-	waw.build = function(root, page){
-		fs.mkdirSync(path.join(root, 'dist'), { recursive: true });
-		if (!fs.existsSync(path.join(root, 'index.html'))) {
-			return console.log('Missing index.html in template '+ root +' root folder');
+	waw.build = function (root, page) {
+		fs.mkdirSync(path.join(root, "dist"), { recursive: true });
+		const indexPath = fs.existsSync(path.join(root, "base.html"))
+			? path.join(root, "base.html")
+			: path.join(root, "index.html");
+
+		if (!fs.existsSync(indexPath)) {
+			return console.log(
+				"Missing index.html in template " + root + " root folder"
+			);
 		}
-		if (!fs.existsSync(path.join(root, 'pages', page))) {
-			return console.log('In template ' + root + ' missing page ' + page);
+		if (!fs.existsSync(path.join(root, "pages", page))) {
+			return console.log("In template " + root + " missing page " + page);
 		}
-		let code = fs.readFileSync(root+sep+'index.html', 'utf8');
-		if (fs.existsSync(path.join(root, 'pages', page, 'index.scss')) ||
-			fs.existsSync(path.join(root, 'pages', page, page+'.scss'))) {
-			let cssLink = '<link rel="stylesheet" href="'+template.prefix+'/css/'+page+'.css"></link>';
-			code = code.replace('<style><!-- CSS --></style>', cssLink, 'utf8');
-		}else if (fs.existsSync(path.join(root, 'pages', page, 'index.css'))) {
-			code = code.replace('<!-- CSS -->', fs.readFileSync(path.join(root, 'pages', page, 'index.css'), 'utf8'));
-		}else if (fs.existsSync(path.join(root, 'pages', page, page+'.css'))) {
-			code = code.replace('<!-- CSS -->', fs.readFileSync(path.join(root, 'pages', page, page+'.css'), 'utf8'));
-		}else code = code.replace('<!-- CSS -->', '');
+		let code = fs.readFileSync(indexPath, "utf8");
+		if (
+			fs.existsSync(path.join(root, "pages", page, "index.scss")) ||
+			fs.existsSync(path.join(root, "pages", page, page + ".scss"))
+		) {
+			let cssLink =
+				'<link rel="stylesheet" href="' +
+				template.prefix +
+				"/css/" +
+				page +
+				'.css"></link>';
+			code = code.replace("<style><!-- CSS --></style>", cssLink, "utf8");
+		} else if (fs.existsSync(path.join(root, "pages", page, "index.css"))) {
+			code = code.replace(
+				"<!-- CSS -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, "index.css"),
+					"utf8"
+				)
+			);
+		} else if (
+			fs.existsSync(path.join(root, "pages", page, page + ".css"))
+		) {
+			code = code.replace(
+				"<!-- CSS -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, page + ".css"),
+					"utf8"
+				)
+			);
+		} else code = code.replace("<!-- CSS -->", "");
 
-		if (fs.existsSync(path.join(root, 'pages', page, 'index.html'))) {
-			code = code.replace('<!-- HTML -->', fs.readFileSync(path.join(root, 'pages', page, 'index.html'), 'utf8'));
-		}else if (fs.existsSync(path.join(root, 'pages', page, page+'.html'))) {
-			code = code.replace('<!-- HTML -->', fs.readFileSync(path.join(root, 'pages', page, page+'.html'), 'utf8'));
-		}else code = code.replace('<!-- HTML -->', '');
+		if (fs.existsSync(path.join(root, "pages", page, "index.html"))) {
+			code = code.replace(
+				"<!-- HTML -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, "index.html"),
+					"utf8"
+				)
+			);
+		} else if (
+			fs.existsSync(path.join(root, "pages", page, page + ".html"))
+		) {
+			code = code.replace(
+				"<!-- HTML -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, page + ".html"),
+					"utf8"
+				)
+			);
+		} else code = code.replace("<!-- HTML -->", "");
 
-		if (fs.existsSync(path.join(root, 'pages', page, 'index.js'))) {
-			code = code.replace('<!-- JS -->', fs.readFileSync(path.join(root, 'pages', page, 'index.js'), 'utf8'));
-		}else if (fs.existsSync(path.join(root, 'pages', page, page+'.js'))) {
-			code = code.replace('<!-- JS -->', fs.readFileSync(path.join(root, 'pages', page, page+'.js'), 'utf8'));
-		}else code = code.replace('<!-- JS -->', '');
+		if (fs.existsSync(path.join(root, "pages", page, "index.js"))) {
+			code = code.replace(
+				"<!-- JS -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, "index.js"),
+					"utf8"
+				)
+			);
+		} else if (
+			fs.existsSync(path.join(root, "pages", page, page + ".js"))
+		) {
+			code = code.replace(
+				"<!-- JS -->",
+				fs.readFileSync(
+					path.join(root, "pages", page, page + ".js"),
+					"utf8"
+				)
+			);
+		} else code = code.replace("<!-- JS -->", "");
 
-		fs.writeFileSync(path.join(root, 'dist', page+'.html'), code, 'utf8');
-	}
-}
+		fs.writeFileSync(path.join(root, "dist", page + ".html"), code, "utf8");
+	};
+};
--- runner.js
+++ runner.js
@@ -1,33 +1,81 @@
-const fs = require('fs');
-const path = require('path');
-const new_page = function(waw){
+const fs = require("fs");
+const path = require("path");
+const scripts = require("./index");
+const new_page = function (waw) {
 	waw.argv.shift();
-	if(!waw.argv.length){
-		console.log('Provide Name');
+	if (!waw.argv.length) {
+		console.log("Provide Name");
 		process.exit(0);
 	}
 	let name = waw.argv[0].toLowerCase();
 	let Name = name.slice(0, 1).toUpperCase() + name.slice(1);
-	let location = path.join(process.cwd(), 'pages', name);
-	
+	let location = path.join(process.cwd(), "pages", name);
+
 	if (fs.existsSync(location)) {
-		console.log('Page already exists');
+		console.log("Page already exists");
 		process.exit(0);
 	}
 	fs.mkdirSync(location, { recursive: true });
-	
-	let code = fs.readFileSync(__dirname + '/page/index.html', 'utf8');
-	code = code.split('CNAME').join(Name);
-	code = code.split('NAME').join(name);
-	fs.writeFileSync(path.join(location, name+'.html'), code, 'utf8');
-	
-	code = fs.readFileSync(__dirname+'/page/page.json', 'utf8');
-	code = code.split('CNAME').join(Name);
-	code = code.split('NAME').join(name);
-	fs.writeFileSync(path.join(location,'page.json'), code, 'utf8');
-	
-	console.log('Page has been created');
+
+	let code = fs.readFileSync(__dirname + "/page/index.html", "utf8");
+	code = code.split("CNAME").join(Name);
+	code = code.split("NAME").join(name);
+	fs.writeFileSync(path.join(location, name + ".html"), code, "utf8");
+
+	code = fs.readFileSync(__dirname + "/page/page.json", "utf8");
+	code = code.split("CNAME").join(Name);
+	code = code.split("NAME").join(name);
+	fs.writeFileSync(path.join(location, "page.json"), code, "utf8");
+
+	console.log("Page has been created");
 	process.exit(1);
-}
+};
 module.exports.page = new_page;
 module.exports.p = new_page;
+
+const build = async function (waw) {
+	if (!fs.existsSync(process.cwd(), "template.json")) {
+		console.log(
+			"Looks like this is not waw template project, I cannot build it"
+		);
+		process.exit(1);
+	}
+	const wjst = require(path.join(waw._modules.sem.__root, "wjst"));
+	const sem = require(path.join(waw._modules.sem.__root, "index"));
+	const core = require(path.join(waw._modules.core.__root, "index"));
+	core(waw);
+	sem(waw);
+	wjst(waw);
+	scripts(waw);
+	if (!fs.existsSync(process.cwd(), "base.html")) {
+		console.log(
+			"Looks like you don't base.html, please rename index.html into base.html"
+		);
+		process.exit(1);
+	}
+	const folders = waw.getDirectories(path.join(process.cwd(), "pages"));
+	const templateJson = waw.readJson(
+		path.join(process.cwd(), "template.json")
+	);
+	for (const folder of folders) {
+		const page = path.basename(folder);
+		waw.build(process.cwd(), page);
+		const json = {
+			...templateJson,
+			...waw.readJson(
+				path.join(process.cwd(), "pages", page, "page.json")
+			),
+		};
+		fs.writeFileSync(
+			path.join(process.cwd(), page + '.html'),
+			waw.wjst.compileFile(
+				path.join(process.cwd(), "dist", page + ".html")
+			)(json),
+			"utf8"
+		);
+	}
+	console.log("Template is builded");
+	process.exit(1);
+};
+module.exports.build = build;
+module.exports.b = build;
