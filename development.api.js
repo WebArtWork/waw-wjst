e85f7b690cc71e995ed0b031ae3ebfcb8a4ca762
Honchar Denys <crackeraki@gmail.com>
2025-09-02 22:25:26 +0300

    added proper generate management

--- development.api.js
+++ development.api.js
@@ -4,199 +4,208 @@ const sass = require("sass");
 const chokidar = require("chokidar");
 
 module.exports = async function (waw) {
-  await waw.wait(1000);
+	await waw.wait(1000);
 
-  let wjst = {};
-  if (fs.existsSync(path.join(process.cwd(), "wjst.json"))) {
-    wjst = JSON.parse(fs.readFileSync(path.join(process.cwd(), "wjst.json")));
-  } else return;
-  waw.now = Date.now();
-  waw.serve(process.cwd(), {
-    prefix: wjst.prefix,
-  });
-  fs.mkdirSync(path.join(process.cwd(), "pages"), {
-    recursive: true,
-  });
-  waw.app.get("/reset", function (req, res) {
-    res.json(waw.now || "");
-  });
-  const compileScss = async (root, file, name) => {
-    if (file.endsWith(".scss") && fs.existsSync(path.join(root, file))) {
-      const result = await sass.compile(path.join(root, file), {
-        style: "compressed",
-      });
-      fs.writeFile(
-        path.join(process.cwd(), "css", name),
-        result.css,
-        "utf8",
-        (err) => {
-          if (err) throw err;
-          waw.now = Date.now();
-        }
-      );
-    }
-  };
-  compileScss(path.join(process.cwd(), "css"), "index.scss", "index.css");
-  /*
-   *	Pages Management
-   */
-  let pages = waw.getDirectories(path.join(process.cwd(), "pages"));
-  const serve = function (page) {
-    let url = "/" + ((page.name != "index" && page.name) || "");
-    waw.app.get(url, function (req, res) {
-      page.config.translate = (slug) => {
-        return slug.split(".").slice(1).join(".");
-      };
-      if (Array.isArray(wjst.functions)) {
-        for (const func of wjst.functions) {
-          page.config[func] = (param) => {
-            return param;
-          };
-        }
-      }
-      let html = waw._derer.renderFile(page.dist, page.config);
-      let refresh = `<script>var id, reset = ()=>{ fetch('/reset').then(response => response.json()).then(resp => { if(!id) id = resp; else if(id != resp){ return location.reload(); }; setTimeout(reset, 1000); });};reset();</script>`;
-      html = html.replace("</body>", refresh + "</body>");
-      res.send(html);
-    });
-    waw.build(process.cwd(), page.name);
-    const pagesRoot = path.join(process.cwd(), "pages", page.name);
-    compileScss(pagesRoot, "index.scss", page.name + ".css");
-    chokidar
-      .watch(page.root, {
-        recursive: true,
-      })
-      .on("all", (action, file) => {
-        compileScss(pagesRoot, file, page.name + ".css");
-        waw.afterWhile(
-          this,
-          () => {
-            waw.build(process.cwd(), page.name);
-            waw.now = Date.now();
-          },
-          100
-        );
-      });
-  };
+	let wjst = {};
+	if (fs.existsSync(path.join(process.cwd(), "wjst.json"))) {
+		wjst = JSON.parse(
+			fs.readFileSync(path.join(process.cwd(), "wjst.json"))
+		);
+	} else return;
+	waw.now = Date.now();
+	waw.serve(process.cwd(), {
+		prefix: wjst.prefix,
+	});
+	fs.mkdirSync(path.join(process.cwd(), "pages"), {
+		recursive: true,
+	});
+	waw.app.get("/reset", function (req, res) {
+		res.json(waw.now || "");
+	});
+	const compileScss = async (root, file, name) => {
+		if (file.endsWith(".scss") && fs.existsSync(path.join(root, file))) {
+			const result = await sass.compile(path.join(root, file), {
+				style: "compressed",
+			});
+			fs.writeFile(
+				path.join(process.cwd(), "css", name),
+				result.css,
+				"utf8",
+				(err) => {
+					if (err) throw err;
+					waw.now = Date.now();
+				}
+			);
+		}
+	};
+	compileScss(path.join(process.cwd(), "css"), "index.scss", "index.css");
+	/*
+	 *	Pages Management
+	 */
+	let pages = waw.getDirectories(path.join(process.cwd(), "pages"));
+	const serve = function (page) {
+		let url = "/" + ((page.name != "index" && page.name) || "");
+		waw.app.get(url, function (req, res) {
+			page.config.translate = (slug) => {
+				return slug.split(".").slice(1).join(".");
+			};
+			if (Array.isArray(wjst.functions)) {
+				for (const func of wjst.functions) {
+					page.config[func] = (param) => {
+						return param;
+					};
+				}
+			}
+			let html = waw._derer.renderFile(page.dist, page.config);
+			let refresh = `<script>var id, reset = ()=>{ fetch('/reset').then(response => response.json()).then(resp => { if(!id) id = resp; else if(id != resp){ return location.reload(); }; setTimeout(reset, 1000); });};reset();</script>`;
+			html = html.replace("</body>", refresh + "</body>");
+			res.send(html);
+		});
+		waw.build(process.cwd(), page.name);
+		const pagesRoot = path.join(process.cwd(), "pages", page.name);
+		compileScss(pagesRoot, "index.scss", page.name + ".css");
+		chokidar
+			.watch(page.root, {
+				recursive: true,
+			})
+			.on("all", (action, file) => {
+				compileScss(pagesRoot, file, page.name + ".css");
+				waw.afterWhile(
+					this,
+					() => {
+						waw.build(process.cwd(), page.name);
+						waw.now = Date.now();
+					},
+					100
+				);
+			});
+	};
 
-  for (let i = pages.length - 1; i >= 0; i--) {
-    let root = pages[i];
-    pages[i] = pages[i].split(path.sep).pop();
-    let name = pages[i];
-    const pagesPageJsonRoot = path.join(
-      process.cwd(),
-      "pages",
-      name,
-      "page.json"
-    );
-    if (fs.existsSync(pagesPageJsonRoot)) {
-      pages[i] = {
-        config: JSON.parse(JSON.stringify(wjst)),
-        dist: process.cwd() + "/dist/" + name + ".html",
-        root: root,
-        name: name,
-      };
-      let page = JSON.parse(fs.readFileSync(pagesPageJsonRoot));
-      for (let each in page) {
-        pages[i].config[each] = page[each];
-      }
-      pages[i].config.page = `/pages/${name}/${name}.html`;
-      serve(pages[i]);
-    } else {
-      pages.splice(i, 1);
-    }
-  }
-  let loc = false;
-  const reset = function (action, file) {
-    if (loc) return;
-    waw.afterWhile(
-      this,
-      () => {
-        for (var i = 0; i < pages.length; i++) {
-          waw.build(process.cwd(), pages[i].name);
-        }
-        waw.now = Date.now();
-        loc = true;
-        setTimeout(() => {
-          loc = false;
-        }, 2000);
-      },
-      100
-    );
-  };
-  chokidar
-    .watch(process.cwd() + "/index.html", {
-      recursive: true,
-    })
-    .on("all", reset);
-  chokidar
-    .watch(process.cwd() + "/base.html", {
-      recursive: true,
-    })
-    .on("all", reset);
-  chokidar
-    .watch(process.cwd() + "/wjst.json", {
-      recursive: true,
-    })
-    .on("all", reset);
-  chokidar
-    .watch(
-      process.cwd() + "/js",
-      {
-        recursive: true,
-      },
-      reset
-    )
-    .on("all", reset);
-  chokidar
-    .watch(process.cwd() + "/css", {
-      recursive: true,
-    })
-    .on("all", (action, file) => {
-      if (file.endsWith(".scss")) {
-        compileScss(process.cwd() + "/css", "index.scss", "index.css");
-      } else {
-        reset();
-      }
-    });
-  chokidar
-    .watch(process.cwd() + "/img", {
-      recursive: true,
-    })
-    .on("all", reset);
-  /*
-   *	Proxy Management
-   */
-  const http = waw.config.api
-    ? waw.http(waw.config.api, waw.config.apiPort || 443)
-    : null;
-  waw.use((req, res, next) => {
-    if (req.originalUrl.startsWith("/api/") && waw.config.api) {
-      http.headers = req.headers;
-      if (req.method === "GET") {
-        http.get(req.originalUrl, (data) => {
-          if (typeof data === "object") {
-            res.setHeader("Content-Type", "application/json");
-            res.json(data);
-          } else {
-            res.setHeader("Content-Type", "application/javascript");
-            res.send(data);
-          }
-        });
-      } else {
-        http[req.method.toLowerCase()](req.originalUrl, req.body, (data) => {
-          if (typeof data === "object") {
-            res.setHeader("Content-Type", "application/json");
-            res.json(data);
-          } else {
-            res.setHeader("Content-Type", "application/javascript");
-            res.send(data);
-          }
-        });
-      }
-    } else {
-      next();
-    }
-  });
-  /* End of */
+	for (let i = pages.length - 1; i >= 0; i--) {
+		let root = pages[i];
+		pages[i] = pages[i].split(path.sep).pop();
+		let name = pages[i];
+		const pagesPageJsonRoot = path.join(
+			process.cwd(),
+			"pages",
+			name,
+			"page.json"
+		);
+		if (fs.existsSync(pagesPageJsonRoot)) {
+			pages[i] = {
+				config: JSON.parse(JSON.stringify(wjst)),
+				dist: process.cwd() + "/dist/" + name + ".html",
+				root: root,
+				name: name,
+			};
+			let page = JSON.parse(fs.readFileSync(pagesPageJsonRoot));
+			for (let each in page) {
+				pages[i].config[each] = page[each];
+			}
+			pages[i].config.page = `/pages/${name}/${name}.html`;
+			serve(pages[i]);
+		} else {
+			pages.splice(i, 1);
+		}
+	}
+	let loc = false;
+	const reset = function (action, file) {
+		if (loc) return;
+		waw.afterWhile(
+			this,
+			() => {
+				for (var i = 0; i < pages.length; i++) {
+					waw.build(process.cwd(), pages[i].name);
+				}
+				waw.now = Date.now();
+				loc = true;
+				setTimeout(() => {
+					loc = false;
+				}, 2000);
+			},
+			100
+		);
+	};
+	chokidar
+		.watch(process.cwd() + "/index.html", {
+			recursive: true,
+		})
+		.on("all", reset);
+	chokidar
+		.watch(process.cwd() + "/base.html", {
+			recursive: true,
+		})
+		.on("all", reset);
+	chokidar
+		.watch(process.cwd() + "/wjst.json", {
+			recursive: true,
+		})
+		.on("all", reset);
+	chokidar
+		.watch(
+			process.cwd() + "/js",
+			{
+				recursive: true,
+			},
+			reset
+		)
+		.on("all", reset);
+	chokidar
+		.watch(process.cwd() + "/css", {
+			recursive: true,
+		})
+		.on("all", (action, file) => {
+			if (file.endsWith(".scss")) {
+				compileScss(process.cwd() + "/css", "index.scss", "index.css");
+			} else {
+				reset();
+			}
+		});
+	chokidar
+		.watch(process.cwd() + "/img", {
+			recursive: true,
+		})
+		.on("all", reset);
+	/*
+	 *	Proxy Management
+	 */
+	const http = waw.config.api
+		? waw.http(waw.config.api, waw.config.apiPort || 443)
+		: null;
+	waw.use((req, res, next) => {
+		if (req.originalUrl.startsWith("/api/") && waw.config.api) {
+			http.headers = req.headers;
+			if (req.method === "GET") {
+				http.get(req.originalUrl, (data) => {
+					if (typeof data === "object") {
+						res.setHeader("Content-Type", "application/json");
+						res.json(data);
+					} else {
+						res.setHeader("Content-Type", "application/javascript");
+						res.send(data);
+					}
+				});
+			} else {
+				http[req.method.toLowerCase()](
+					req.originalUrl,
+					req.body,
+					(data) => {
+						if (typeof data === "object") {
+							res.setHeader("Content-Type", "application/json");
+							res.json(data);
+						} else {
+							res.setHeader(
+								"Content-Type",
+								"application/javascript"
+							);
+							res.send(data);
+						}
+					}
+				);
+			}
+		} else {
+			next();
+		}
+	});
+	/* End of */
 };
--- runner.js
+++ runner.js
@@ -1,327 +1,356 @@
 const fs = require("fs");
 const path = require("path");
 const scripts = require("./index.api");
+const configJsPath = path.join(process.cwd(), "config.js");
+const configJs = fs.existsSync(configJsPath) ? require(configJsPath) : {};
 
 const getWjstJson = (waw) => {
-  let wjstJson = waw.readJson(path.join(process.cwd(), "wjst.json"));
+	let wjstJson = waw.readJson(path.join(process.cwd(), "wjst.json"));
 
-  if (waw.config.fetch) {
-    for (const docs in waw.config.fetch) {
-      try {
-        wjstJson = {
-          ...wjstJson,
-          ...waw.readJson(path.join(process.cwd(), docs + ".json")),
-        };
-      } catch (error) {
-        console.error(`using fetch for ${docs}: ${error}`);
-      }
-    }
-  }
+	if (waw.config.fetch) {
+		for (const docs in waw.config.fetch) {
+			try {
+				wjstJson = {
+					...wjstJson,
+					...waw.readJson(path.join(process.cwd(), docs + ".json")),
+				};
+			} catch (error) {
+				console.error(`using fetch for ${docs}: ${error}`);
+			}
+		}
+	}
 
-  return wjstJson;
+	return wjstJson;
 };
 
 const new_page = function (waw) {
-  waw.argv.shift();
-  if (!waw.argv.length) {
-    console.log("Provide Name");
-    process.exit(0);
-  }
-  let name = waw.argv[0].toLowerCase();
-  let Name = name.slice(0, 1).toUpperCase() + name.slice(1);
-  let location = path.join(process.cwd(), "pages", name);
+	waw.argv.shift();
+	if (!waw.argv.length) {
+		console.log("Provide Name");
+		process.exit(0);
+	}
+	let name = waw.argv[0].toLowerCase();
+	let Name = name.slice(0, 1).toUpperCase() + name.slice(1);
+	let location = path.join(process.cwd(), "pages", name);
 
-  if (fs.existsSync(location)) {
-    console.log("Page already exists");
-    process.exit(0);
-  }
-  fs.mkdirSync(location, { recursive: true });
+	if (fs.existsSync(location)) {
+		console.log("Page already exists");
+		process.exit(0);
+	}
+	fs.mkdirSync(location, { recursive: true });
 
-  let code = fs.readFileSync(__dirname + "/page/index.html", "utf8");
-  code = code.split("CNAME").join(Name);
-  code = code.split("NAME").join(name);
-  fs.writeFileSync(path.join(location, name + ".html"), code, "utf8");
+	let code = fs.readFileSync(__dirname + "/page/index.html", "utf8");
+	code = code.split("CNAME").join(Name);
+	code = code.split("NAME").join(name);
+	fs.writeFileSync(path.join(location, name + ".html"), code, "utf8");
 
-  code = fs.readFileSync(__dirname + "/page/page.json", "utf8");
-  code = code.split("CNAME").join(Name);
-  code = code.split("NAME").join(name);
-  fs.writeFileSync(path.join(location, "page.json"), code, "utf8");
+	code = fs.readFileSync(__dirname + "/page/page.json", "utf8");
+	code = code.split("CNAME").join(Name);
+	code = code.split("NAME").join(name);
+	fs.writeFileSync(path.join(location, "page.json"), code, "utf8");
 
-  console.log("Page has been created");
-  process.exit();
+	console.log("Page has been created");
+	process.exit();
 };
 module.exports.page = new_page;
 module.exports.p = new_page;
 
 async function generate_documents(waw, exit = true) {
-  if (waw.config.generate) {
-    if (exit) {
-      const wjst = require(path.join(waw._modules.sem.__root, "wjst"));
-      const sem = require(path.join(waw._modules.sem.__root, "index"));
-      const core = require(path.join(waw._modules.core.__root, "index"));
-      core(waw);
-      sem(waw);
-      wjst(waw);
-      scripts(waw);
-    }
+	if (!Array.isArray(waw.config.generate)) {
+		if (exit) {
+			console.warn("There is no configuration to generate content");
 
-    for (const _url in waw.config.generate) {
-      let url = _url.split("/");
+			process.exit();
+		} else {
+			return;
+		}
+	}
 
-      let file = url.pop();
+	if (exit) {
+		const wjst = require(path.join(waw._modules.sem.__root, "wjst"));
+		const sem = require(path.join(waw._modules.sem.__root, "index"));
+		const core = require(path.join(waw._modules.core.__root, "index"));
+		core(waw);
+		sem(waw);
+		wjst(waw);
+		scripts(waw);
+	}
 
-      const folder = url.length ? url.join("/") : "";
+	const wjstJson = waw.readJson(path.join(process.cwd(), "wjst.json"));
 
-      if (folder) {
-        const folderPath = path.join(process.cwd(), folder);
+	for (const generate of waw.config.generate) {
+		if (
+			!generate.items ||
+			!generate.config ||
+			!generate.page ||
+			!wjstJson[generate.items]
+		)
+			continue;
 
-        if (!fs.existsSync(folderPath)) {
-          fs.mkdirSync(folderPath);
-        }
-      }
+		waw.build(process.cwd(), generate.page);
 
-      const json = getWjstJson(waw);
+		const template = waw.wjst.compileFile(
+			path.join(process.cwd(), "dist", generate.page + ".html")
+		);
 
-      const page = waw.config.generate[_url];
+		for (let document of wjstJson[generate.items]) {
+			document = !!configJs[generate.config]
+				? configJs[generate.config](document)
+				: document;
 
-      waw.build(process.cwd(), page);
+			if (!fs.existsSync(document.__folderPath)) {
+				fs.mkdirSync(document.__folderPath);
+			}
 
-      if (file.startsWith(":")) {
-        file = file.split(":")[1].split(".");
+			fs.writeFileSync(
+				path.join(
+					document.__folderPath,
+					(document.__filePath || document.url || document._id) +
+						".html"
+				),
+				template({
+					...wjstJson,
+					...waw.readJson(
+						path.join(
+							process.cwd(),
+							"pages",
+							generate.page,
+							"page.json"
+						)
+					),
+					...document,
+					page:
+						"/pages/" +
+						generate.page +
+						"/" +
+						generate.page +
+						".html",
+				}),
+				"utf8"
+			);
+		}
+	}
 
-        if (file.length === 2 && Array.isArray(json[file[0]])) {
-          for (const doc of json[file[0]]) {
-            fs.writeFileSync(
-              path.join(process.cwd(), folder, doc[file[1]] + ".html"),
-              waw.wjst.compileFile(
-                path.join(process.cwd(), "dist", page + ".html")
-              )({
-                ...json,
-                ...doc,
-              }),
-              "utf8"
-            );
-          }
-        } else if (exit) {
-          console.error("Bad configuration for url " + _url);
+	console.log("Your files been generated");
 
-          process.exit();
-        }
-      } else {
-        fs.writeFileSync(
-          path.join(process.cwd(), folder, page + ".html"),
-          waw.wjst.compileFile(
-            path.join(process.cwd(), "dist", page + ".html")
-          )(json),
-          "utf8"
-        );
-      }
-    }
-
-    console.log("Your files been generated");
-  } else {
-    console.log("You don't have any generate configuration");
-  }
-  if (exit) {
-    process.exit();
-  }
+	if (exit) {
+		process.exit();
+	}
 }
 
 module.exports.generate = generate_documents;
 module.exports.g = generate_documents;
 
 async function fetchInfo(url) {
-  try {
-    const response = await fetch(url);
+	try {
+		const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
 
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error.message);
-  }
+		return await response.json();
+	} catch (error) {
+		console.error("Error:", error.message);
+	}
 }
 
 const fetch_documents = async function (waw, exit = true) {
-  if (!waw.config.fetch) {
-    if (exit) {
-      console.warn("There is no configuration to fetch content");
+	if (!waw.config.fetch) {
+		if (exit) {
+			console.warn("There is no configuration to fetch content");
 
-      process.exit();
-    } else {
-      return;
-    }
-  }
+			process.exit();
+		} else {
+			return;
+		}
+	}
 
-  const wjstJson = getWjstJson(waw);
+	const wjstJson = getWjstJson(waw);
 
-  for (const config of waw.config.fetch) {
-    wjstJson[config.wjst] = await fetchInfo(config.api);
-  }
+	for (const config of waw.config.fetch) {
+		wjstJson[config.wjst] = await fetchInfo(config.api);
+	}
 
-  fs.writeFileSync(
-    path.join(process.cwd(), "wjst.json"),
-    JSON.stringify(wjstJson, null, 4)
-  );
+	fs.writeFileSync(
+		path.join(process.cwd(), "wjst.json"),
+		JSON.stringify(wjstJson, null, 4)
+	);
 
-  if (exit) {
-    console.log("Information has been fetched");
+	if (exit) {
+		console.log("Information has been fetched");
 
-    process.exit();
-  }
+		process.exit();
+	}
 };
 module.exports.fetch = fetch_documents;
 module.exports.f = fetch_documents;
 
 const build = async function (waw) {
-  if (!fs.existsSync(process.cwd(), "wjst.json")) {
-    console.log("Looks like this is not waw wjst project, I cannot build it");
-    process.exit(1);
-  }
+	if (!fs.existsSync(process.cwd(), "wjst.json")) {
+		console.log(
+			"Looks like this is not waw wjst project, I cannot build it"
+		);
+		process.exit(1);
+	}
 
-  const wjst = require(path.join(waw._modules.sem.__root, "wjst"));
-  const sem = require(path.join(waw._modules.sem.__root, "index"));
-  const core = require(path.join(waw._modules.core.__root, "index"));
-  core(waw);
-  sem(waw);
-  wjst(waw);
-  scripts(waw);
-  if (!fs.existsSync(process.cwd(), "base.html")) {
-    console.log(
-      "Looks like you don't base.html, please rename index.html into base.html"
-    );
-    process.exit(1);
-  }
+	const wjst = require(path.join(waw._modules.sem.__root, "wjst"));
+	const sem = require(path.join(waw._modules.sem.__root, "index"));
+	const core = require(path.join(waw._modules.core.__root, "index"));
+	core(waw);
+	sem(waw);
+	wjst(waw);
+	scripts(waw);
+	if (!fs.existsSync(process.cwd(), "base.html")) {
+		console.log(
+			"Looks like you don't base.html, please rename index.html into base.html"
+		);
+		process.exit(1);
+	}
 
-  await fetch_documents(waw, false);
+	await fetch_documents(waw, false);
 
-  const wjstJson = getWjstJson(waw);
+	const wjstJson = getWjstJson(waw);
 
-  const folders = waw.getDirectories(path.join(process.cwd(), "pages"));
-  for (const folder of folders) {
-    const page = path.basename(folder);
-    waw.build(process.cwd(), page);
-    const json = {
-      ...wjstJson,
-      ...waw.readJson(path.join(process.cwd(), "pages", page, "page.json")),
-      page: `/pages/${page}/${page}.html`,
-      ...(waw.config.build || {}),
-    };
-    fs.writeFileSync(
-      path.join(process.cwd(), page + ".html"),
-      waw.wjst.compileFile(path.join(process.cwd(), "dist", page + ".html"))(
-        json
-      ),
-      "utf8"
-    );
-  }
+	const folders = waw.getDirectories(path.join(process.cwd(), "pages"));
+	for (const folder of folders) {
+		const page = path.basename(folder);
+		waw.build(process.cwd(), page);
+		const json = {
+			...wjstJson,
+			...waw.readJson(
+				path.join(process.cwd(), "pages", page, "page.json")
+			),
+			page: `/pages/${page}/${page}.html`,
+			...(waw.config.build || {}),
+		};
+		fs.writeFileSync(
+			path.join(process.cwd(), page + ".html"),
+			waw.wjst.compileFile(
+				path.join(process.cwd(), "dist", page + ".html")
+			)(json),
+			"utf8"
+		);
+	}
 
-  if (waw.config.build && waw.config.build.page) {
-    if (
-      typeof waw.config.build.page === "object" &&
-      !Array.isArray(waw.config.build.page)
-    ) {
-      waw.config.build.page = [waw.config.build.page];
-    }
+	if (waw.config.build && waw.config.build.page) {
+		if (
+			typeof waw.config.build.page === "object" &&
+			!Array.isArray(waw.config.build.page)
+		) {
+			waw.config.build.page = [waw.config.build.page];
+		}
 
-    for (const page of waw.config.build.page) {
-      if (!page.json || !page.name || (!page.folder && !page.url)) {
-        continue;
-      }
+		for (const page of waw.config.build.page) {
+			if (!page.json || !page.name || (!page.folder && !page.url)) {
+				continue;
+			}
 
-      const localJson =
-        typeof page.json === "string"
-          ? JSON.parse(
-              await fs.readFileSync(
-                path.join(process.cwd(), page.json + ".json"),
-                "utf8"
-              )
-            )
-          : page.json;
+			const localJson =
+				typeof page.json === "string"
+					? JSON.parse(
+							await fs.readFileSync(
+								path.join(process.cwd(), page.json + ".json"),
+								"utf8"
+							)
+					  )
+					: page.json;
 
-      if (page.folder) {
-        const folder = path.join(process.cwd(), page.folder);
+			if (page.folder) {
+				const folder = path.join(process.cwd(), page.folder);
 
-        if (!fs.existsSync(folder)) {
-          fs.mkdirSync(folder);
-        }
+				if (!fs.existsSync(folder)) {
+					fs.mkdirSync(folder);
+				}
 
-        for (const doc of localJson[page.json]) {
-          const json = {
-            ...wjstJson,
-            ...waw.readJson(
-              path.join(process.cwd(), "pages", page.name, "page.json")
-            ),
-            ...(waw.config.build || {}),
-            ...doc,
-          };
-          fs.writeFileSync(
-            path.join(folder, (doc.nameUrl || doc._id) + ".html"),
-            waw.wjst.compileFile(
-              path.join(process.cwd(), "dist", page.name + ".html")
-            )(json),
-            "utf8"
-          );
-        }
-      } else if (page.url) {
-        const json = {
-          ...wjstJson,
-          ...waw.readJson(
-            path.join(process.cwd(), "pages", page.name, "page.json")
-          ),
-          ...(waw.config.build || {}),
-          ...localJson,
-        };
-        fs.writeFileSync(
-          path.join(process.cwd(), page.url + ".html"),
-          waw.wjst.compileFile(
-            path.join(process.cwd(), "dist", page.name + ".html")
-          )(json),
-          "utf8"
-        );
-      }
-    }
-  }
+				for (const doc of localJson[page.json]) {
+					const json = {
+						...wjstJson,
+						...waw.readJson(
+							path.join(
+								process.cwd(),
+								"pages",
+								page.name,
+								"page.json"
+							)
+						),
+						...(waw.config.build || {}),
+						...doc,
+					};
+					fs.writeFileSync(
+						path.join(folder, (doc.nameUrl || doc._id) + ".html"),
+						waw.wjst.compileFile(
+							path.join(
+								process.cwd(),
+								"dist",
+								page.name + ".html"
+							)
+						)(json),
+						"utf8"
+					);
+				}
+			} else if (page.url) {
+				const json = {
+					...wjstJson,
+					...waw.readJson(
+						path.join(
+							process.cwd(),
+							"pages",
+							page.name,
+							"page.json"
+						)
+					),
+					...(waw.config.build || {}),
+					...localJson,
+				};
+				fs.writeFileSync(
+					path.join(process.cwd(), page.url + ".html"),
+					waw.wjst.compileFile(
+						path.join(process.cwd(), "dist", page.name + ".html")
+					)(json),
+					"utf8"
+				);
+			}
+		}
+	}
 
-  await generate_documents(waw, false);
+	await generate_documents(waw, false);
 
-  console.log("Template is builded");
-  process.exit();
+	console.log("Template is builded");
+	process.exit();
 };
 module.exports.build = build;
 module.exports.b = build;
 
 const _remove = (fileOrFolder) => {
-  fileOrFolder = path.join(process.cwd(), fileOrFolder);
+	fileOrFolder = path.join(process.cwd(), fileOrFolder);
 
-  if (fs.existsSync(fileOrFolder)) {
-    const stat = fs.lstatSync(fileOrFolder);
+	if (fs.existsSync(fileOrFolder)) {
+		const stat = fs.lstatSync(fileOrFolder);
 
-    if (stat.isDirectory()) {
-      fs.rmSync(fileOrFolder, { recursive: true });
-    } else {
-      fs.rmSync(fileOrFolder);
-    }
-  }
+		if (stat.isDirectory()) {
+			fs.rmSync(fileOrFolder, { recursive: true });
+		} else {
+			fs.rmSync(fileOrFolder);
+		}
+	}
 };
 const remove = async function (waw) {
-  _remove("pages");
+	_remove("pages");
 
-  _remove("dist");
+	_remove("dist");
 
-  _remove("base.html");
+	_remove("base.html");
 
-  if (waw.config.remove) {
-    for (let fileOrFolder of waw.config.remove) {
-      _remove(fileOrFolder);
-    }
-  }
+	if (waw.config.remove) {
+		for (let fileOrFolder of waw.config.remove) {
+			_remove(fileOrFolder);
+		}
+	}
 
-  console.log("Files and folders been removed");
+	console.log("Files and folders been removed");
 
-  process.exit();
+	process.exit();
 };
 module.exports.remove = remove;
 module.exports.r = remove;
